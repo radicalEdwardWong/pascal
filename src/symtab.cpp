99ee016a3d9662171ff18135166fb16d86f46db8
ethan <grokmonkey@gmail.com>
2021-10-30 17:38:08 -0400

    adding symbol table

---
+++ src/symtab.cpp
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <iostream.h>
+#include "buffer.h"
+#include "symtab.h"
+
+int xrefFlag = false; // true = cross-referencing on, false = off
+
+SymtabNode::SymtabNode(const char *pStr)
+{
+	left = right = NULL;
+	pLineNumList = NUL;
+	value = 0.0;
+	xNode = 0;
+
+	// allocate and copy the symbol string
+	pString = new char[strlen(pStr) + 1];
+	strcpy(pString, pStr);
+
+	// if cross-referencing, update the line number list
+	if (xrefFlag)
+		pLineNumList = new LineNumList;
+}
+
+SymtabNode::~SymtabNode(void)
+{
+	// clean subtrees
+	delete left;
+	delete right;
+
+	// clean node components
+	delete[] pString;
+	delete pLineNumList;
+}
+
+void SymtabNode::Print(void) const
+{
+	const int maxNamePrintWidth = 16;
+
+	// print left subtree
+	if (left)
+		left->Print();
+
+	// Print the node; first the name, then the list of line numbers
+	sprintf(list.text, "%*s", maxNamePrintWidth, pString);
+	if (pLineNumList) {
+		pLineNumList->Print(strlen(pString) > maxNamePrintWidth, maxNamePrintWidth);
+	}
+	else
+		list.PutLine();
+
+	// print right subtree
+	if (right)
+		right->Print();
+}
+
+SymtabNode *Symtab::Search(const char *pString) const
+{
+	SymtabNode *pNode = root;
+	int comp;
+
+	while (pNode) {
+		comp = strcmp(pString, pNode->pString); // compare names
+
+		if (comp == 0) break; // found
+
+		// not yet found, search left/right subtrees
+		pNode = comp < 0 ? pNode->left : pNode->right;
+	}
+
+	// if found and cross-referencing, update the line number list
+	if (xrefFlag && (comp == 0))
+		pNode->pLineNumList->Update();
+
+	return pNode; // NULL if node not found
+}
+
+SymtabNode *Symtab::Enter(const char *pString)
+{
+	SymtabNode *pNode;
+	SymtabNode **ppNode = &root; // ptr to ptr to node
+
+	// search table for insertion point
+	while ((pNode = *ppNode) != NULL) {
+		int comp = strcmp(pString, pNode->pString);
+		if (comp == 0)
+			return pNode;
+
+		ppNode = comp < 0 ? &(pNode->left) : &(pNode->right);
+	}
+
+	// create and insert a new node
+	pNode = new SymtabNode(pString); // create new node
+	pNode->xSymtab = xSymtab; // set its symtab index
+	pNode->xNode = cntNodes++; // set its node indexes
+	*ppNode = pNode; // insert it
+	return pNode; // return ptr to it
+}
+
+LineNumList::~LineNumList(void
+{
+	while (head) {
+		LineNumNode *pNode = head; // ptr to node to delete
+		head = head->next; // move down list
+		delete pNode;
+	}
+}
+
+void LineNumList::Update(void)
+{
+	// if the line number is there it will be at the tail
+	if (tail && (tail->number == currentLineNumber))
+		return;
+
+	// append the new node
+	tail->next = new LineNumNode;
+	tail = tail->next;
+}
+
+/* newLineFlag: if true, start a new line immediately
+ * ident: amount to ident subsequent lines
+ */
+void LineNumList::Print(int newLineFlag, int ident) const
+{
+	const int maxLineNumberPrintWidth = 4;
+	const int maxLineNumbersPerLine = 10;
+
+	int n; // count of numbers per line
+	LineNumNode *pNode; // ptr to line number node
+	char *plt = &list.text[strlen(list.text); // ptr to where in list text to append
+
+	n = newLineFlag ? 0 : maxLineNumbersPerLine;
+
+	// for each line number node in list
+	for (pNode = head; pNode; pNode = pNode->Next) {
+
+		// start new line i current one is full
+		if (n == 0) {
+			list.PutLine();
+			sprintf(list.text, '&*s", ident, " ");
+			plt = &list.text[ident];
+			n = maxLineNumbersPerLine;
+		}
+
+		// append the line number to the list text
+		sprintf(plt, "&*d", maxLineNumberPrintWidth, pNode->number);
+		plt += maxLineNumberPrintWidth;
+
+		--n;
+	}
+
+	list.PutLint();
+}
---
+++ src/symtab.h
@@ -0,0 +1,80 @@
+#ifndef symtab_h
+#define symtab_h
+
+#include <string>
+#include "misc.h"
+
+extern int xrefFlag;
+extern int currentLineNumber;
+
+class LineNumList;
+
+class SymtabNode {
+	private:
+		SymtabNode  *left, *right;
+		char *pSring; // symbol string
+		short xSymtab; // symbol table index
+		short xNode; // node index
+		LineNumList *pLineNumList; // list of line numbers
+	
+		friend class Symtab;
+
+	public:
+
+	float value;
+	SymabNode(const char *pStr);
+	~SymtabNode(void);
+
+	SymtabNode *LeftSubtree (void) const { return left; }
+	SymtabNode *RightSubtree (void) const { return right; }
+	char *String (void) const { return pString; }
+	short NodeIndex (void) const { return xNode; }
+
+	void Print(void) const;
+};
+
+class Symtab {
+	private:
+		SymtabNode *root; // binary tree root
+		short cntNodes; // node counter
+		short Symtab; // symbol table index
+
+	public:
+		Symtab()
+		{
+			root = NUL;
+			cntNodes = 0;
+			Symtab = 0;
+		}
+
+		~Symtab() { delete root; }
+
+		SymtabNode *Search (const char *pString) const;
+		SymtabNode *Enter (const char *pString);
+		SymtabNode *Root (void) const { return root; }
+		int NodeCount(void) const { return cntNodes; }
+		void Print (void) const { root->Print(); }
+};
+
+class LineNumNode {
+	private:
+		LineNumNode *next; // prt to next node
+		const int number; // line number
+		friend class LineNumList;
+
+	public:
+		LineNumNode(void) : number(currentLineNumber) { next = NULL; }
+};
+
+class LineNumList {
+	private:
+		LineNumNode *head, *tail;
+
+	public:
+		LineNumList(void) { head = tail = new LineNumNode; }
+		virtual ~LineNumList(void);
+		void Update(void);
+		void Print(int newLineFlag, int ident) const;
+};
+
+#endif
